ec7ebb075701300b52f749895d88f91bf337474f
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-03-26 10:12:16 +0200

    Add files via upload

---
+++ src/DerCommander610/TPA/Main.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace KoDe\Commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+class TPA extends PluginBase{
+
+    private $TPA = [];
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+         switch($command->getName()) {
+
+             case "tpa":
+                 if(!$sender instanceof Player){
+                     $sender->sendMessage("Du kannst diesen befehl nur Ingame verwenden!");
+                     return false;
+                 }
+                 if(isset($args[0])){
+                     $sender->sendMessage("§cDu musst den Spielernamen eingeben um den eine TPA anfrage schicken zu können!");
+                     $event->setMessage("§cDu musst den Spielernamen eingeben um den eine TPA anfrage schicken zu können!");
+                     @mkdir($this->getDataFolder());
+                     $this->getResource("tpa.yml")
+                     if($sender->setMessage($this->getConfig()->get("dont-writed-playername-message")))
+                     return false;
+                 }
+
+                 $player = $this->getServer()->getPlayer($args[0]);
+
+                 if(!$player){
+                     $sender->sendMessage("§cDieser Spieler ist nicht Online!");
+                     $event->setMessage("§cDieser Spieler ist nicht Online!");
+                     @mkdir($this->getDataFolder());
+                     $this->getResource("tpa.yml")
+                     if($sender->setMessage($this->getConfig()->get("player-not-found-message")))
+                     return false;
+                 }
+                 if($sender->getName() == $player->getName()){
+                     $sender->sendMessage("§cNetter versuch!");
+                     $event->setMessage("§cNetter versuch!");
+                     @mkdir($this->getDataFolder());
+                     $this->getResource("tpa.yml")
+                     if($sender->setMessage($this->getConfig()->get("giving-himself")))
+                         return false;
+                 }
+
+
+                 $this->TPA[$player->getName()] = $sender->getName();
+                 $sender->sendMessage("§aDu hast erfolgreich eine TPA geschickt!");
+                 $player->sendMessage("§aDu hast eine TPA anfrage erhalten! mach /tpaccept um es anzunehmen! mach /tpadecline um die anfrage abzulehen!");
+                 break;
+
+             case "tpaccept":
+                     if(!$sender instanceof Player) {
+                         $sender->sendMessage("Du kannst diesen befehl nur Ingame verwenden!");
+                         return false;
+                     }
+
+                     if(!isset($this->TPA[$sender->getName()])){
+                         $sender->sendMessage("$cDu hast noch keine TPA`s erhalten!");
+                         return false;
+                     }
+
+                     $player = $this->getServer()->getPlayer($this->TPA[$sender->getName()]);
+
+                   if(!$player) {
+                       $sender->sendMessage("§cDer Spieler der dir eine TPA geschickt hat ist Offline! Sein TPA ist nun ungültig!");
+                       return false;
+                     }
+
+                 $player = $this->getServer()->getPlayer($args[0]);
+
+                 if(!$player){
+                     $sender->sendMessage("§cDieser Spieler ist nicht Online!");
+                     return false;
+                     }
+
+                  unset($this->TPA[$sender->getName()]);
+                      $player->teleport($sender->getPosition());
+                      $sender->sendMessage("§aDu hast" . $sender->getName() . "`s TPA anfrage erfolgreich angenommen!");
+                      $player->sendMessage("§e" . $sender->getName() . "§a hat deine TPA angenommen!");
+                  break;
+
+                  case "tpadecline":
+                      if(!$sender instanceof Player) {
+                          $sender->sendMessage("Du kannst diesen befehl nur Ingame verwenden!");
+                          return false:
+
+                          {
+
+                      $player = $this->getServer()->getPlayer($this->TPA[$sender->getName()]);
+
+                      if(!$player) {
+                          $sender->sendMessage("§cDer Spieler der dir eine TPA geschickt hat den du ablehen kannst ist zurzeit Offline gegangen! Sein TPA ist nun ungültig!");
+                          return false;
+                      }
+
+                      unset($this->TPA[$sender->getName()]);
+                      $sender->sendMessage("§cDu hast§e" . $sender->getName() . "`s §cTPA erfolgreich abgelehnt!");
+                      $player->sendMessage("§e" . $sender->getName() . "§chat deine TPA abgelehnt!!");
+                      break;
+        }
+        return true;
+    }
+}
